85ed22947ead07e1f70405fdd4ea79d0189bd134
FelipeAndriFe <felipeaferreira06@gmail.com>
2025-04-28 17:17:54 -0300

    Add files via upload

---
+++ Teste/teste.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main ()
+{
+    int b;
+    int h;
+    int A;
+    std::cout << "Input base";
+    std::cin >> b;
+    std::cout << "Input height";
+    std::cin >> h;
+    A = b * h;
+    printf("%d" , A);
+}
